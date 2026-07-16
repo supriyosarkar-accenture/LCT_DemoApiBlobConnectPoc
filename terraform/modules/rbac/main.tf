232b6ec01e2de74d48cf825dc306d33bab0a26db
supriyosarkar-accenture <supriyo.a.sarkar@accenture.com>
2026-07-17 03:30:26 +0530

    bug fix

--- terraform/modules/rbac/main.tf
+++ terraform/modules/rbac/main.tf
@@ -2,6 +2,4 @@ resource "azurerm_role_assignment" "this" {
   scope                = var.scope
   principal_id         = var.principal_id
   role_definition_name = var.role_definition_name
-
-  skip_service_principal_aad_check = true
 }
